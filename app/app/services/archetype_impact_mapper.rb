847d6cc88b5a30934f0f41e53df9126010d384cd
Matt Vo <t.v.tmv@hotmail.com>
2026-04-24 17:03:26 -0700

    Update how impacts are calculated

--- app/app/services/archetype_impact_mapper.rb
+++ app/app/services/archetype_impact_mapper.rb
@@ -4,31 +4,47 @@ class ArchetypeImpactMapper
       matched_tags = matching_tags(archetype, patch_change)
       next if matched_tags.empty?
 
-      raw_score = matched_tags.count.to_f
+      raw_score = score_match(archetype, matched_tags)
       signed_score = negative_change?(patch_change.change_type) ? -raw_score : raw_score
 
-      ArchetypeImpact.create!(
+      impact = ArchetypeImpact.find_or_initialize_by(
         archetype: archetype,
-        patch_change: patch_change,
-        impact_score: signed_score,
-        impact_kind: classify_impact(signed_score),
-        reasoning: "Matched tags: #{matched_tags.join(', ')}"
+        patch_change: patch_change
       )
+
+      impact.impact_score = signed_score
+      impact.impact_kind = classify_impact(signed_score)
+      impact.reasoning = "Matched tags: #{matched_tags.join(', ')}"
+      impact.save!
+
+      impact
     end
   end
 
+  def self.score_match(archetype, matched_tags)
+    score = 0.0
+
+    matched_tags.each do |tag|
+      score += 1.5 if Array(archetype.core_mechanics).include?(tag)
+      score += 1.0 if Array(archetype.offense_tags).include?(tag)
+      score += 0.75 if Array(archetype.defense_tags).include?(tag)
+    end
+
+    score
+  end
+
   def self.matching_tags(archetype, patch_change)
-    change_tags = patch_change.tags || []
+    change_tags = normalize_tags(patch_change.tags).map(&:to_s)
     archetype_tags =
-      (archetype.offense_tags || []) +
-      (archetype.defense_tags || []) +
-      (archetype.core_mechanics || [])
+      Array(archetype.offense_tags).map(&:to_s) +
+      Array(archetype.defense_tags).map(&:to_s) +
+      Array(archetype.core_mechanics).map(&:to_s)
 
     change_tags & archetype_tags
   end
 
   def self.negative_change?(change_type)
-    %w[nerf removed].include?(change_type)
+    %w[nerf removed].include?(change_type.to_s)
   end
 
   def self.classify_impact(score)
@@ -38,5 +54,17 @@ class ArchetypeImpactMapper
     return "indirect_nerf" if score.negative?
 
     "neutral"
+  end
+
+  def self.normalize_tags(tags)
+    Array(tags).map(&:to_s).flat_map do |tag|
+      case tag.downcase
+      when "projectile_scaling" then ["projectile_scaling", "projectile"]
+      when "minion_scaling" then ["minion_scaling", "minion"]
+      when "attack_speed" then ["attack_speed", "attack"]
+      else
+        [tag.downcase]
+      end
+    end.uniq
   end
 end
